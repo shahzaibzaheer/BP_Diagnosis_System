d8dc6b80a7ede408b36229f6be5202c24e99effc
Salman Asghar <salmanasgharghulam@gmail.com>
2019-04-11 03:46:00 +0500

    some changes

--- public/patient/doctors.php
+++ public/patient/doctors.php
@@ -10,9 +10,9 @@
     <h1>Doctors</h1>
 
       <div class="doctors-container">
-        <div class="row d-flex justify-content-around  gutter grid-column-gap-1">
+        <div class="row d-flex justify-content-around  gutter grid-column-gap-1 col-md-12 mx-auto">
           <?php foreach ($doctors as $doctor): ?>
-            <div  class="card col-8 col-sm-5 col-md-4 text-center  " style="padding:0;"  onclick="location.href='<?php echo urlFor('patient/doctor_detail.php')."?doctor_id=".$doctor->getId(); ?>';">
+            <div  class="card col-8 col-sm-5 col-md-3 text-center mt-4 ml-1 mr-1 " style="padding:0;"  onclick="location.href='<?php echo urlFor('patient/doctor_detail.php')."?doctor_id=".$doctor->getId(); ?>';">
               <img class="card-img img-fluid" src="../../private/shared/assets/default_img.png" alt="">
               <h5 class="card-title text-uppercase"><?php echo $doctor->getName(); ?></h5>
               <div class="card-subtitle"><?php echo $doctor->getQualification(); ?></div>
