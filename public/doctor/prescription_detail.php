f9861e3b5172c98fe50287c3d9a13d65d7000e9c
Zaheer <zaheer.mughal1965@gmail.com>
2019-04-11 23:05:21 +0500

    Doctor's Prescription Detail UI completed

--- public/doctor/prescription_detail.php
+++ public/doctor/prescription_detail.php
@@ -22,7 +22,7 @@
         $prescription->setStatus(Prescription::STATUS_ANSWERED);
         // exit("Doctor Id: ".loggedInDoctorId());
         if($prescription->save()){
-          exit("doctor_id successfully updated");
+          redirectTo($_SERVER['SCRIPT_NAME']."?prescription_id=".$prescriptionId);
         }else {
           print_array($prescription->getErrors());
           exit;
@@ -46,47 +46,54 @@
     }else {
       exit("User don't pass the id, redirect where it should be");
     }
+    require_once(getSharedFilePath('doctor/header.php'));
 ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Prescription Detail</title>
-  </head>
-  <body>
-    <h1>Prescription Detail</h1>
 
+    <section class="main_content message_container">
+      <h1>Prescription Detail</h1>
+      <div class="message-wrapper">
+        <div class="message from">
+          <p class="meta-info">Created On: <b><?php echo $prescription->getCreatedOn(); ?></b></p>
+              <p>
+                <b>Subject: </b> <?php echo $prescription->getSubject(); ?> <br>
+                <b>BP Reading: </b> <?php echo $prescription->getBpLow(). " low, ".$prescription->getBpHigh()." high "; ?> <br>
+                <b>Headache: </b> <?php echo  $prescription->getHeadache()=='1'?  "Yes": "No"; ?> <br>
+                <b>Any Visual Change: </b> <?php echo $prescription->getVisualChanges(); ?> <br>
+                <b>Dizziness: </b> <?php echo $prescription->getDizziness()=='1'?  "Yes": "No"; ?> <br>
+                <b>Medication: </b> <?php echo $prescription->getMedication(); ?> <br>
+                <b>Food Detail: </b> <?php echo $prescription->getFoodDetail(); ?> <br>
+                <b>Exercise Detail: </b> <?php echo $prescription->getExerciseDetail(); ?> <br>
+                <b>Other Information: </b> <?php echo $prescription->getOtherInfo(); ?> <br>
+              </p>
+        </div>
 
-    <p><b>Patient Name: </b><?php echo $prescription->getPatientName();  ?></p>
-    <p><b>Prescription Id: </b> <?php echo $prescription->getPrescriptionId(); ?></p>
-    <p><b>Doctor Name: </b> <?php echo $prescription->getDoctorName(); ?> </p>
-    <p><b>Prescription Status: </b> <?php echo $prescription->getPrescriptionId(); ?> </p>
-    <p><b>Subject: </b> <?php echo $prescription->getSubject(); ?> </p>
-    <p><b>LOW BP: </b> <?php echo $prescription->getBpLow(); ?> </p>
-    <p><b>High BP: </b> <?php echo $prescription->getBpHigh(); ?> </p>
-    <p><b>headache: </b> <?php echo $prescription->getHeadache(); ?> </p>
-    <p><b>Dizziness: </b> <?php echo $prescription->getDizziness(); ?> </p>
-    <p><b>Any visual changes: </b> <?php echo $prescription->getVisualChanges(); ?> </p>
-    <p><b>Medication: </b> <?php echo $prescription->getMedication(); ?> </p>
-    <p><b>Food Detail: </b> <?php echo $prescription->getFoodDetail(); ?> </p>
-    <p><b>Exercise Detail: </b> <?php echo $prescription->getExerciseDetail(); ?> </p>
-    <p><b>Other Info: </b> <?php echo $prescription->getOtherInfo(); ?> </p>
-    <p><b>Posted at: </b> <?php echo $prescription->getCreatedOn(); ?> </p>
+        <?php
+          $replies = Replay::find_replies_by_prescription_id($prescription->getPrescriptionId());
+          if($replies){?>
+              <!-- Output All the replies  -->
+              <?php foreach ($replies as $reply):
+                 if($reply->isDoctorReplay()){?> <!-- If is doctor's reply -->
+                 <div class="message to">
+                   <p class="meta-info">created on: <b><?php echo $reply->getCreatedOn(); ?></b> </p>
+                   <p><?php echo $reply->getReplayMessage(); ?></p>
+                 </div>
+               <?php }else{?>
+                 <div class="message from">
+                   <p class="meta-info">created on: <b><?php echo $reply->getCreatedOn(); ?></b> </p>
+                   <p><?php echo $reply->getReplayMessage(); ?></p>
+                 </div>
+                 <?php } ?>
 
-    <!-- Output all the replyes -->
-    <?php
-        $replies = Replay::find_replies_by_prescription_id($prescriptionId);
-        print_array($replies);
-    ?>
-    <div class="doc_replay">
+              <?php endforeach; ?>
+          <?php } ?>
+          <!-- Visible the replay box to doctor -->
+      </div>
+      <div class="replay-box">
+        <form action="<?php echo $_SERVER['SCRIPT_NAME']."?prescription_id=".$prescription->getPrescriptionId(); ?>" method="post">
+          <input class="form-control" type="text" name="replayMessage" value="" placeholder="Enter Your Replay">
+          <button class="btn btn-primary" type="submit"  name="button"> <i class="fas fa-paper-plane"></i> </button>
+        </form>
+      </div>
+    </section>
 
-    </div>
-
-
-    <br><br><br><br>
-    <form action="<?php echo $_SERVER['SCRIPT_NAME']."?prescription_id=".$prescription->getPrescriptionId(); ?>" method="post">
-        <input type="text" name="replayMessage" value="" placeholder="Replay" required>
-        <input type="Submit" name="submit" value="submit">
-    </form>
-  </body>
-</html>
+<?php     require_once(getSharedFilePath('doctor/footer.php')); ?>
