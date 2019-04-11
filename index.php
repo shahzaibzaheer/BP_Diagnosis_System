5eee8336df0f2882861fbeec959391a1377c76da
Salman Asghar <salmanasgharghulam@gmail.com>
2019-04-11 11:22:13 +0500

    Main Home UI completed

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php  require_once('private/initialize.php');
+    redirectTo('/BP_Diagnosis_System/public/index.php');
+ ?>
--- public/index.php
+++ public/index.php
@@ -5,25 +5,127 @@
     <meta charset="utf-8">
     <title>Home</title>
     <style media="screen">
-      *{
-        font-family: sans-serif;
-      }
-      a{
-        text-decoration: none;
-        color: #fff;
-        background: #333;
-        padding: 8px;
-        border-radius: 5px;
-      }
+    .carousel > img{
+      width : 100%;
+      height: 80%;
+    }
     </style>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo getStylePath("main.css"); ?>">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Home</h1>
-        <a href="<?php echo urlFor('patient/login.php') ?>">Patient Login</a>
-        <a href="<?php echo urlFor('patient/registration.php') ?>">Patient Registration</a> <br><br><br>
-        <a href="<?php echo urlFor('doctor/login.php') ?>">Doctor Login</a>
-        <a href="<?php echo urlFor('doctor/registration.php') ?>">Doctor Registration</a><br><br><br>
-        <a href="<?php echo urlFor('admin/registration.php') ?>">Admin Registration</a>
-        <a href="<?php echo urlFor('admin/login.php') ?>">Admin Login</a>
+
+    <!-- Navigation -->
+     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+       <div class="container">
+         <a class="navbar-brand" href="index.html">Blood Pressure Diagnosis System</a>
+         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarResponsive">
+           <ul class="navbar-nav ml-auto mr-5">
+             <li class="nav-item">
+               <a class="nav-link active" href="about.html">Home</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="services.html">About Us</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="contact.html">Registration</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="contact.html">Login</a>
+             </li>
+           </ul>
+         </div>
+       </div>
+     </nav>
+<!-- ../private/shared/assets/default_img.png -->
+     <header>
+        <div id="carouselControl" class="carousel slide" data-ride="carousel">
+         <div class="carousel-inner">
+           <div class="carousel-item active">
+             <img class="d-block w-100 " src="https://placehold.it/900x400" alt="First slide">
+           </div>
+           <div class="carousel-item">
+             <img class="d-block w-100" src="https://placehold.it/900x400" alt="Second slide">
+           </div>
+           <div class="carousel-item">
+             <img class="d-block w-100" src="https://placehold.it/900x400" alt="Third slide">
+           </div>
+         </div>
+         <a class="carousel-control-prev" href="#carouselControl" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Previous</span>
+         </a>
+         <a class="carousel-control-next" href="#carouselControl" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Next</span>
+         </a>
+        </div>
+     </header>
+
+       <!-- Page Content -->
+       <div class="container mt-5">
+
+          <div class="row text-center">
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">Lorem Ipsum</a>
+                  </h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">Lorem Ipsum</a>
+                  </h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">Lorem Ipsum</a>
+                  </h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /.row -->
+
+
+       </div>
+
+
+
+
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+        <script type="text/javascript">
+          $(function()){
+            $('.carousel').carousel(['interval'=>500]);
+          }
+        </script>
   </body>
+
 </html>
