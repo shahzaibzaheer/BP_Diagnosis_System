737d71527d2384ed2c27b0bf17f2f0228f0e4a6c
Zaheer <zaheer.mughal1965@gmail.com>
2019-04-25 01:21:00 +0500

    Prescription detail ui fixes

--- public/patient/my_prescription_detail.php
+++ public/patient/my_prescription_detail.php
@@ -76,15 +76,20 @@
                  <?php } ?>
 
               <?php endforeach; ?>
+            </div>
 
               <!-- Visible the replay box to the user -->
-              <div class="replay-box">
+              <div class="replay-box col-12 ">
                 <form action="<?php echo $_SERVER['SCRIPT_NAME']."?prescription_id=".$prescription->getPrescriptionId(); ?>" method="post">
                   <input class="form-control" type="text" name="replayMessage" value="" placeholder="Enter Your Replay">
                   <button class="btn btn-primary" type="submit"  name="button"> <i class="fas fa-paper-plane"></i> </button>
                 </form>
               </div>
           <?php } ?>
-
-
-<?php     require_once(getSharedFilePath('patient/footer.php')); ?>
+        </section>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="<?php echo getScriptPath('main_script.js'); ?>"></script>
+      </body>
+</html>
